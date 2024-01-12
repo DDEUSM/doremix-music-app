da4cb9a53b4a3e266aa61aee07da674abac4e6c9
unknown <daviddeusmesquita@gmail.com>
2024-01-12 19:58:31 -0300

    feat(discover): Add music genres carrousel to Discover page.

--- src/pages/discover/components/music-genres-slider/index.tsx
+++ src/pages/discover/components/music-genres-slider/index.tsx
@@ -1,14 +1,18 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/pagination";
 import "./music-genres-slider.scss";
+
+import { Pagination } from "swiper/modules";
 
 export default function MusicGenres()
 {
     return (
-        <div className="">
+        <div className="genres_slider">
             <Swiper
-                slidesPerView={3}
+                slidesPerView={5}
                 spaceBetween={20}
+                modules={[Pagination]}
                 className="mySwiper"
             >
                 <SwiperSlide>1</SwiperSlide>
